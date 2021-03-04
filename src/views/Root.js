38c248813d593a494f6b7cbe454d4d4fb4dd1d0e
Wojciech Kaliszewski <wkaliszewski@sii.pl>
2021-03-04 17:47:11 +0100

    Add users list

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,25 +1,17 @@
-import logo from 'assets/logo.svg';
-import 'views/App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsersList from 'components/UsersList/UsersList';
 
-function App() {
+const Root = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React dddd
-        </a>
-      </header>
-    </div>
+    <>
+      <UsersList />
+    </>
   );
-}
+};
+
+Root.propTypes = {
+  
+};
 
 export default App;
